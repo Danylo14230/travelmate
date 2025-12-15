82b1b6fcbfa75e01c3e13b5009cc504c7953bc7f
Danylo14230 <[danylo14230@gmail.com]>
2025-12-16 00:56:40 +0200

    Fix Android flavors + Gradle Kotlin DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,30 +1,43 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
+    id("kotlin-android")
+
+    // Flutter
+    id("dev.flutter.flutter-gradle-plugin")
+
+    // Firebase
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-flavorDimensions "environment"
-
-productFlavors {
-    dev {
-        dimension "environment"
-        applicationIdSuffix ".dev"
-        versionNameSuffix "-dev"
-    }
-    prod {
-        dimension "environment"
-    }
 
     namespace = "com.example.travel_organizer"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
+    // ✅ FLAVORS
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
+
+        create("prod") {
+            dimension = "environment"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.travel_organizer"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -32,24 +45,12 @@ productFlavors {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.travel_organizer"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        jvmTarget = "11"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // ⚠️ для CI нормально
             signingConfig = signingConfigs.getByName("debug")
         }
     }
