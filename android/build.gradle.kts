7538d6b45bafc50f4a479820945efd0c10f3d7cf
Danylo14230 <[danylo14230@gmail.com]>
2025-12-16 00:49:02 +0200

    build.gradle update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,15 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+android {
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
+
+        create("prod") {
+            dimension = "environment"
+        }
     }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
