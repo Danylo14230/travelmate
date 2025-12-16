4c5fb83c8c74eef70fd0a76088077ae1b4e7f4f3
Danylo14230 <[danylo14230@gmail.com]>
2025-12-16 02:18:18 +0200

    android/build.gradle.kts Update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.Delete
+import java.io.File
+
 buildscript {
     repositories {
         google()
@@ -14,4 +17,23 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+/**
+ * ✅ ВАЖЛИВО ДЛЯ FLUTTER:
+ * переносимо build output в кореневий ./build
+ * інакше flutter build інколи не знаходить apk (особливо з flavors) і падає.
+ */
+rootProject.buildDir = File(rootDir, "../build")
+
+subprojects {
+    buildDir = File(rootProject.buildDir, name)
+}
+
+subprojects {
+    evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
